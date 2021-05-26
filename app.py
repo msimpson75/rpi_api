d207495e63cb8a308b77593d0ed6aad4db2eb955
msimpson75 <mike.simpson.07@gmail.com>
2021-05-26 11:22:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route('/time-tracker', methods=['POST'])
+def json_example():
+	request_data = request.get_json()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! And Stuff</p>"
+	activity = request_data['data']['currentTracking']['activity']['name']
+
+	rawTime = request_data['data']['currentTracking']['startedAt']
+
+	time = rawTime
+
+	return '''
+            The activity is: {}
+            The time started is: {}'''.format(activity, time)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
