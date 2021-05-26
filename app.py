6d9d7c0d7497a36b808575e1ae3f5b373e8c8702
msimpson75 <mike.simpson.07@gmail.com>
2021-05-26 11:13:07 -0500

    listener

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/time-tracker', methods=['POST'])
+def json_example():
+	request_data = request.get_json()
+
+	activity = request_data['data']['currentTracking']['activity']['name']
+
+	rawTime = request_data['data']['currentTracking']['startedAt']
+
+	time = rawTime
+
+	return '''
+            The activity is: {}
+            The time started is: {}'''.format(activity, time)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
